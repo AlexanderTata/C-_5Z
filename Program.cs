b889e310462d724b87b07d907b469c7a05b8869a
Alexander Tata <ta1996@yandex.ru>
2022-06-27 15:34:36 +0300

    Решение задачи 5

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿/*Задача 5. Напишите программу, которая на вход принмает число N, а выход все числа от -N до N
+-4 -3 -2 -1 0 1 2 3 4*/
+
+int number = 4;
+int count = -number;
+while (count <= number)
+{
+    Console.Write(count + "  ");
+    count++;
+}
